b5d9a2817d95d5a3b2a494b483f2d5d85bcaa2a9
Sameer Kohli <skohli1508@gmail.com>
2024-09-12 16:14:54 +0100

    adding skeleton and updating Readme - trying to be more thorough with updates

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Managing the game state includes saving progress and updating the DOM.
 
 ---12/09/2024-1526---
 setting up my files and following the walk through talk through skeleton. Hvae some ideas of what I want to do need to remember to hit the requirements and plan on paper what I want to do then build!!!
+
+---12/09/2024-1610---
+Completed the inital skeleton. Need to go away and think of my layout and how I want to structure the script.
+
+- inital want to change names of items in the shop but first need to actually work out if thats possible to do the API.
+- look up local storage again - want to make sure I can confirm what I want to do.
+- MEDIA QURIES - I need to work them out.
+- Write down my structure and my plan on paper - can change on the fly.
+  Just remember Rocky
--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 console.log(`Welcome to the Republic`);
+
+//we need to store 2 global values: banana count and BpS
+
+let bananaCount = 0;
+let BpS = 0;
+
+let stats = {
+  bananaCount: 0,
+  BpS: 0,
+};
+// we can save either as an object or as variables both ave pros and cons for me I am leaning towards 2 different variables since I find that easier to manipulate.
+
+//Dom Manipulation
+//Select the DOM elements (buttons, imgs, p, ...)
+
+//a way to store the shop upgrades that come from the API
+let shopUpgrades = []; //store as an array since it is the same as the API.
+
+function getShopUpgrades() {
+  //fetch the data
+  //translate into JSON
+  //PUSH the items the shopUpgrades array above
+}
+
+//an event listner to click on the banana button.
+//select the banana button/img
+//write yyour event handler and event listner
+
+function handleBananaClick() {
+  //when i click on this the value of the banana counter goes up by 1
+}
+
+addEventListener("click", handleBananaClick);
+
+//we need a timer that increases the Banana Count value by one every second.
+setInterval(function () {
+  //I want to incearse the value of bananaCount by one every second
+  //I want to update the value displayed on the page.
+  //Update cookieCount
+  //I want to store this as a local value.
+}, 1000);
+
+function renderShopUpgrades() {
+  //create DOM elements
+  //you will use a loop or array mehtod
+  shopUpgrades.forEach(function (upgraede) {
+    //for eacj item in the array, assign the value to a DOM element
+    //append the elelment to the DOM
+  });
+}
