1cec68fb2225d365b37661d8cdc5dc4f1c4ca894
Sameer Kohli <skohli1508@gmail.com>
2024-09-13 15:05:29 +0100

    making skeleton work- still got work to do

--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Completed the inital skeleton. Need to go away and think of my layout and how I 
 Spent some time planning how i want my page to look, I ahve all the elements I need in place and an image need to change font and some minor styling but will do for now. Shock since I planned it went a lot smoother than before. Also added a simple counter - bit janky need to fix but does the basics. Need to plan my code again properly. Overall good start I think.
 
 Going to watch Rocky now...
+
+13/09/22024 1458
+
+Spent all day working on it. Spent time with Manny to work out how API nomcleture works, definitly got my head round it so comfortable now implementing it. Also got my autoclick working with live counter - little bit janky speeds ups massivley when buy things timing issue. Overall the clicker works need to style it a bit more. Also the writing down worked my index.html makes sense and so do my functions since when I showed people they could understand it.
+
+- inital want to change names of items in the shop but first need to actually work out if thats possible to do the API. I can make this work but more important things to do. If i have time will do.
+- look up local storage again - want to make sure I can confirm what I want to do. Looked it up more comfortable need to really work on it.
+- MEDIA QURIES - I need to work them out.Still no clue hahah. Will read more.
+- Write down my structure and my plan on paper - can change on the fly. This works for me. Also allows me to explain things when I am stuck also realised makes changing things on the fly a lot easier.
+- Want to style the table better.
+- also going to experiment with comments when you hit certain goals the code is relativley easy...(famous last words)
+- going better than last week. Having manny and Joe helped alot also ttalking to over people.
+
+Rocky is not really a sports movie...
--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 console.log(`Welcome to the Republic`);
 
 //we need to store 2 global values: banana count and BpS
-
-let bananaCount = 0; // global variable which is setting the count at 0 - will use throughout.
 let BpS = 0; // global variable which is setting the count at 0 - will use throughout.
-let bananaButton = document.getElementById("bananaButton"); // DOM to use in event listner for "clicks"
+let bananaCount = 0 + BpS; // global variable which is setting the count at 0 - will use throughout.
+let bananaAutoClickerButton = document.getElementById(
+  "bananaAutoClickerButton"
+); // DOM to use in event listner for "buy"
 let bananaBpS = document.getElementById("BpS"); //DOM to use in event listner for BpS - going to use a similar format as the button.
+let bananaShop = document.getElementById("bananaShop");
+
+bananaButton.addEventListener("click", handleBananaClick);
 
 function liveBananaCount() {
   //code which shows the value of the banana count and replaces the words.
-  document.getElementById("bananaTotalLocation").textContent = bananaCount;
+  document.getElementById("BananaTotal").textContent = bananaCount;
+}
+function handleBananaClick() {
+  //when i click on this the value of the banana counter goes up by 1
+
+  bananaCount = bananaCount + 1;
+  //   document.getElementById("BananaTotal").textContent = bananaCount;
+  liveBananaCount(); //replacing words with live counter - looks good and is simple to do plus it makes sense in my head to do it this way. Initial problem was when I put it to +1 it was adding 2 to the counter I assume since its running it twice due to live banna count function also using bananaCount as a variable. The pure coder in me would spend 20 minutes fixing this so they wouldn't interfere with each other. The lazy man in me says they should only add 0.5 at a time if it is getting doubled... I assume when I start messing around with BpS I will have to come back and fix this. Heres hoping I don't though. 12/09/24 2024 UPDATE for some reason presson my rest button also adds to the counter but only by 0.5... wierd but I havn't coded it yet to Reset so heres hoping the lazy plan still works. 12/09/2024 2026. UPDATE Really confused now when I click the image it adds +1 as wanted however if I click around the pictue it clicks +0.5 which is wild I have not a clue why suspect its something to do with .innerHTML function but if I take it out the code breaks... 12/09/24 2040 ---UPDATE--- spoke with manny for 20 minutes we could not figure out what was going on he then went away and worked out it was all my notes at the bottom effecting the code. Once I commented the code out it worked exactly how I thought it would and I have the +1 now as well. 13/09/2024 1048.
+}
+
+bananaAutoClickerButton.addEventListener("click", buyAutoClickerButtonHandler);
+
+function buyAutoClickerButtonHandler() {
+  bananaCount = bananaCount - 100;
+  BpS = BpS + 1;
+  setInterval(function () {
+    bananaCount = bananaCount + BpS;
+    console.log(BpS);
+    console.log(bananaCount);
+    liveBananaCount();
+  }, BpS * 1000);
+  liveBpS();
 }
 
 function liveBpS() {
   //code which shows the value of the BpS and replaces the words.
   document.getElementById("BpS").textContent = BpS;
 }
-
-bananaButton.addEventListener("click", handleBananaClick);
-
-function handleBananaClick() {
-  //when i click on this the value of the banana counter goes up by 1
-
-  bananaCount = bananaCount + 1;
-  //   document.getElementById("BananaTotal").textContent = bananaCount;
-  liveBananaCount(); //replacing words with live counter - looks good and is simple to do plus it makes sense in my head to do it this way. Initial problem was when I put it to +1 it was adding 2 to the counter I assume since its running it twice due to live banna count function also using bananaCount as a variable. The pure coder in me would spend 20 minutes fixing this so they wouldn't interfere with each other. The lazy man in me says they should only add 0.5 at a time if it is getting doubled... I assume when I start messing around with BpS I will have to come back and fix this. Heres hoping I don't though. 12/09/24 2024 UPDATE for some reason presson my rest button also adds to the counter but only by 0.5... wierd but I havn't coded it yet to Reset so heres hoping the lazy plan still works. 12/09/2024 2026. UPDATE Really confused now when I click the image it adds +1 as wanted however if I click around the pictue it clicks +0.5 which is wild I have not a clue why suspect its something to do with .innerHTML function but if I take it out the code breaks... 12/09/24 2040
-}
-
-console.log(document.getElementById("BananaTotal").textContent);
 
 async function getShop() {
   //creating api fetch function.
@@ -36,47 +49,71 @@ async function getShop() {
   );
   console.log(response);
 
-  const json = await response.json();
+  const json = await response.json(); //changing daata into jason
   console.log(json);
-}
 
-// let stats = {
-//   bananaCount: 0,
-//   BpS: 0,
-// };
-// we can save either as an object or as variables both ave pros and cons for me I am leaning towards 2 different variables since I find that easier to manipulate.
+  json.forEach(function (a) {
+    //for each function takes the information and places it in the html document.
+    //creat an html element to contain the data from the API
+    //make sure there is an individual element per peice of data
+    //managed to solve it but needed a lot of help, I looked over for each functions again and had to really talk through the process of what was going on - note for future self when doing something like this really think through each step - will try apply same thought process to local storage since I am not very confident with that either at the moment. 13/09/24 1241
+    const shopNames = document.createElement("p");
+    const shopCost = document.createElement("p");
+    const shopBpSIncrease = document.createElement("p");
 
-//Dom Manipulation
-//Select the DOM elements (buttons, imgs, p, ...)
+    shopNames.textContent = a.name;
+    shopCost.textContent = a.cost;
+    shopBpSIncrease.textContent = a.increase;
 
-//a way to store the shop upgrades that come from the API
-let shopUpgrades = []; //store as an array since it is the same as the API.
+    shopNames.className = "shopNames";
+    shopCost.className = "shopCost";
+    shopBpSIncrease.className = "shopBpSIncrease";
 
-function getShopUpgrades() {
-  //fetch the data
-  //translate into JSON
-  //PUSH the items the shopUpgrades array above
-}
-
-//an event listner to click on the banana button.
-//select the banana button/img
-//write yyour event handler and event listner
-
-addEventListener("click", handleBananaClick);
-
-//we need a timer that increases the Banana Count value by one every second.
-setInterval(function () {
-  //I want to incearse the value of bananaCount by one every second
-  //I want to update the value displayed on the page.
-  //Update cookieCount
-  //I want to store this as a local value.
-}, 1000);
-
-function renderShopUpgrades() {
-  //create DOM elements
-  //you will use a loop or array mehtod
-  shopUpgrades.forEach(function (upgraede) {
-    //for eacj item in the array, assign the value to a DOM element
-    //append the elelment to the DOM
+    bananaShop.appendChild(shopNames);
+    bananaShop.appendChild(shopCost);
+    bananaShop.appendChild(shopBpSIncrease);
   });
 }
+
+getShop();
+
+// // let stats = {
+// //   bananaCount: 0,
+// //   BpS: 0,
+// // };
+// // we can save either as an object or as variables both ave pros and cons for me I am leaning towards 2 different variables since I find that easier to manipulate.
+
+// //Dom Manipulation
+// //Select the DOM elements (buttons, imgs, p, ...)
+
+// //a way to store the shop upgrades that come from the API
+// let shopUpgrades = []; //store as an array since it is the same as the API.
+
+// function getShopUpgrades() {
+//   //fetch the data
+//   //translate into JSON
+//   //PUSH the items the shopUpgrades array above
+// }
+
+// //an event listner to click on the banana button.
+// //select the banana button/img
+// //write yyour event handler and event listner
+
+// addEventListener("click", handleBananaClick);
+
+// //we need a timer that increases the Banana Count value by one every second.
+// setInterval(function () {
+//   //I want to incearse the value of bananaCount by one every second
+//   //I want to update the value displayed on the page.
+//   //Update cookieCount
+//   //I want to store this as a local value.
+// }, 1000);
+
+// function renderShopUpgrades() {
+//   //create DOM elements
+//   //you will use a loop or array mehtod
+//   shopUpgrades.forEach(function (upgraede) {
+//     //for eacj item in the array, assign the value to a DOM element
+//     //append the elelment to the DOM
+//   });
+// }
